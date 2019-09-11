b5d11f9bd6f5adf9fc9f25c7e04d0cff7ab0c753
Sazi Mtandabuzo <s_mtandabuzo@yahoo.com>
2019-09-11 21:48:02 +0200

    Delete format.sh

--- format.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# ------------------------------------------------------
-# WebStorm formatting script.
-# ------------------------------------------------------
-#
-
-IDE_BIN_HOME="${0%/*}"
-exec "$IDE_BIN_HOME/../MacOS/webstorm" format "$@"
